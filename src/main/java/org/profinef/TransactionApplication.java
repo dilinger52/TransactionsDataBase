c725e326484b6996788c42e22750c2d61a3faa9d
dilinger <aeg.sasha15@gmail.com>
2023-08-21 14:18:35 +0300

    test v0.3.0beta some bugfixing and improvements

--- src/main/java/org/profinef/TransactionApplication.java
+++ src/main/java/org/profinef/TransactionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
